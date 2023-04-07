c6551b92ea6cb84c199fee1697a9635a31769979
LaStella <jingyu.ham94@gmail.com>
2023-04-07 09:57:12 +0900

    정적 매니저 프로젝트 생성

---
+++ static-manager-service/src/test/java/com/example/staticmanagerservice/StaticManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.staticmanagerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticManagerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
